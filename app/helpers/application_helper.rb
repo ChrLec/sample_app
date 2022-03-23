f916941cf1fcafa5efa926f619eb0480520c7bd1
Christelle Lecamp <christelle.lecamp@gmail.com>
2022-03-23 18:44:55 +0100

    étape cen cours

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title= "")
+    base_title = "ChrLec"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
